a981a6a7163b6f5c3ab9ec7ee35e845f76de4d10
Pavel <player49@mail.ru>
2020-11-18 17:54:38 +0300

    Initial upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,3 +20,10 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+/* h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+*/
